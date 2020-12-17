a03e93f91099f915710023cc8326eda7b196da3b
Kenrich005 <66662380+Kenrich005@users.noreply.github.com>
2020-12-17 22:29:44 +0530

    Update House Price Prediction Kenny.R

--- House Price Prediction Kenny.R
+++ House Price Prediction Kenny.R
@@ -2,7 +2,7 @@ setwd('G:/Imarticus/DSP_Online_2/Project1PropertyPricePrediction/Dataset')
 
 # Initiating the libraries
 library(Amelia) # For accessing the missmap function
-library(dplyr)  
+library(dplyr)
 library(Hmisc) # For using the impute function
 library(ggplot2)
 library(ModelMetrics) # For in built RMSE Calculation
@@ -33,7 +33,7 @@ table(property_train$Lot_Extent)
 char_names = names(property_train[,sapply(property_train, is.character)])
 
 # We have now stored all variables of character datatype in the variable "char_names"
-char_names 
+char_names
 
 # As they are categorical variables, let us convert them into factors.
 property_train[,char_names]=lapply(property_train[,char_names], factor)
@@ -46,24 +46,24 @@ print(summary(property_train[char_names]))
 # Another way to observe the distribution of each categorical variable is:
 
 for (val in char_names){
-  
+
     print(val)
-  
+
     val_table = table(property_train[[val]], useNA = 'ifany')
-    
+
     val_percent = round(prop.table(val_table)*100,2)
-    
+
     print(cbind(val_table, val_percent))
-    
+
     cat("\n")
 }
-  
+
 rm(val,val_percent,val_table)
 
 # Observing the data distribution, we see some highly skewed variables
 # We need to exclude those variables
 
-remove_vals = c("Road_Type", 
+remove_vals = c("Road_Type",
                 "Utility_Type",
                 "Condition2",
                 "Roof_Quality",
@@ -104,7 +104,7 @@ rm(NA_columns)
 # Fence_Quality = 1178
 
 
-filtered_data = subset(filtered_data, 
+filtered_data = subset(filtered_data,
                        select = -c(Lane_Type,
                                    Fireplace_Quality,
                                    Fence_Quality))
@@ -138,7 +138,7 @@ summary(filtered_data$BsmtFinType2)
 
 81/1459
 38/1459
-# Since both the above variables are categorical, 
+# Since both the above variables are categorical,
 # and the number of observations are just about 5%,
 # we choose to omit instead of impute
 
@@ -155,7 +155,7 @@ ggplot(filtered_data, aes(x=Zoning_Class, y=Sale_Price/1000))+
     ylab("Sale Price \n (in thousands)")+
     geom_text(stat = "summary", aes(label=round(stat(y),1)), fun=sum, vjust=-0.3)
 
-ggplot(filtered_data, aes(x=Zoning_Class, y=Sale_Price/1000))+ 
+ggplot(filtered_data, aes(x=Zoning_Class, y=Sale_Price/1000))+
     stat_summary(fun=mean, geom="bar")+
     ylab("Mean Sale Price (in thousands)")+
     geom_text(aes(label=round(stat(y),1)), stat = "summary", fun = mean, vjust = -0.3)
@@ -183,19 +183,19 @@ SP_K = "Sale_Price_K"
 
 for (val in fact_names){
     print(val)
-    
+
     plot_sum = ggplot(filtered_data, aes_string(x=val, y=SP_M))+
         geom_col(fill = "skyblue")+
         ylab("Sale Price \n (in millions)")+
         geom_text(stat = "summary", aes(label=round(stat(y),1)), fun=sum, vjust=-0.3)
-    
+
     print(plot_sum)
-    
-    plot_mean = ggplot(filtered_data, aes_string(x=val, y=SP_K))+ 
+
+    plot_mean = ggplot(filtered_data, aes_string(x=val, y=SP_K))+
         stat_summary(fun=mean, geom="bar", fill = "pink")+
         ylab("Mean Sale Price \n(in thousands)")+
         geom_text(aes(label=round(stat(y),1)), stat = "summary", fun = mean, vjust = -0.3)
-    
+
     print(plot_mean)
 }
 
@@ -254,14 +254,14 @@ summary(stepwise_mode)
 
 # stepwise regression gives us the optimum variable combination for the model
 
-optimum_model = lm(formula = Sale_Price ~ Lot_Extent + Lot_Size + Land_Outline + 
-                     Lot_Configuration + Neighborhood + Condition1 + House_Type + 
-                     House_Design + Overall_Material + House_Condition + Construction_Year + 
-                     Exterior1st + Exterior_Material + Basement_Height + Basement_Condition + 
-                     Exposure_Level + BsmtFinType1 + First_Floor_Area + Second_Floor_Area + 
-                     Underground_Full_Bathroom + Full_Bathroom_Above_Grade + Kitchen_Above_Grade + 
-                     Kitchen_Quality + Rooms_Above_Grade + Functional_Rate + Fireplaces + 
-                     Garage_Size + W_Deck_Area + Enclosed_Lobby_Area + Three_Season_Lobby_Area + 
+optimum_model = lm(formula = Sale_Price ~ Lot_Extent + Lot_Size + Land_Outline +
+                     Lot_Configuration + Neighborhood + Condition1 + House_Type +
+                     House_Design + Overall_Material + House_Condition + Construction_Year +
+                     Exterior1st + Exterior_Material + Basement_Height + Basement_Condition +
+                     Exposure_Level + BsmtFinType1 + First_Floor_Area + Second_Floor_Area +
+                     Underground_Full_Bathroom + Full_Bathroom_Above_Grade + Kitchen_Above_Grade +
+                     Kitchen_Quality + Rooms_Above_Grade + Functional_Rate + Fireplaces +
+                     Garage_Size + W_Deck_Area + Enclosed_Lobby_Area + Three_Season_Lobby_Area +
                      Month_Sold + Sale_Condition, data = train)
 
 summary(optimum_model)
@@ -287,7 +287,39 @@ boxplot(test$Sale_Price/1000,test$predict/1000,
 property_test = read.csv('Property_Price_Test.csv')
 
 property_test$Est_Sale_Price = predict(optimum_model,property_test)
-summary(train$Condition1)
-summary(factor(property_test$Condition1))
+# We notice the error that a new level 'NORMD' is present in the property_test data,
+# which wasn't present in the property_train data.
+
+# Let us drop that particular level.
+# To do that we first need to convert the character datatypes into factors.
+
+property_test <- property_test %>% mutate_if(is.character,as.factor)
+
+#Merging the extra levels in the test data
+levels(property_test$Condition1)
+levels(property_test$Condition1) = c("Artery","Feedr","Norm","Norm","PosA",
+                                     "PosN","RRAe","RRAn","RRNe","RRNn")
+
+# Now let us re-run the prediction again.
+property_test$Est_Sale_Price = predict(optimum_model,property_test)
+
+#Merging the extra levels in the test data
+levels(property_test$Exterior1st)
+levels(property_test$Exterior1st) = c("AsbShng" ,"AsbShng", "BrkComm",
+                                      "BrkFace", "CemntBd", "CemntBd",
+                                      "HdBoard", "MetalSd" ,"Plywood",
+                                      "Stucco","VinylSd","Wd Sdng","WdShing")
 
 
+# Now let us re-run the prediction again.
+property_test$Est_Sale_Price = predict(optimum_model,property_test)
+
+#Merging the extra levels in the test data
+levels(property_test$Sale_Condition)
+levels(property_test$Sale_Condition) = c("Abnorml","Abnorml",
+                                         "AdjLand","Alloca",
+                                         "Family","Normal",
+                                         "Normal","Partial")
+
+# Now let us re-run the prediction again.
+property_test$Est_Sale_Price = predict(optimum_model,property_test)
